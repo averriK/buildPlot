d6bcdf40605d6b973fe689c8c8a91cd916dbbd74
Alejandro Verri <averri@gmail.com>
2024-07-17 12:39:24 -0300

    fix ggplot2

--- inst/test.R
+++ inst/test.R
@@ -1,25 +1,3 @@
 devtools::load_all()
-library(ggplot2)
-library(scales)
-custom_colors <- c("#f1c40f", "#2ecc71", "#9b59b6", "#e74c3c", "#34495e", 
-                   "#3498db", "#1abc9c", "#f39c12", "#d35400")
-
-scale_color_flat <- function(...) {
-  scale_color_manual(values = custom_colors, ...)
-}
-
-scale_fill_flat <- function(...) {
-  scale_fill_manual(values = custom_colors, ...)
-}
-
-# Example of applying the custom theme and color scale
-
-ggplot(mpg, aes(displ, hwy, color = class)) +
-  geom_point(size = 3) +
-  labs(title = "Fuel Efficiency by Engine Size",
-       subtitle = "Data from the ggplot2 package",
-       x = "Engine displacement (liters)",
-       y = "Highway miles per gallon",
-       color = "Car class") +
-  theme_flat() +
-  scale_color_flat()
+DT <- data.frame(ID=iris$Species,X=iris$Sepal.Length,Y=iris$Sepal.Width)
+buildPlot(data=DT,library="ggplot2")
