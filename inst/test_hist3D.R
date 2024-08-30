c8ee19a87b2a79361fc51bcab5968be73bafa442
Alejandro Verri <averri@gmail.com>
2024-08-30 11:30:08 -0300

    fic

--- inst/test_hist3D.R
+++ inst/test_hist3D.R
@@ -1,10 +1,10 @@
 devtools::load_all()
 RMwTable <- readRDS("inst/RMwTable.Rds")
-TR_TARGET <- 10000
+TR_TARGET <- 500
 Tn_TARGET <- 0
 POE_TARGET <- (1 - exp(-50 / TR_TARGET)) |> round(digits = 5)
 # DT <- RMwTable[Tn == Tn_TARGET & POE == POE_TARGET, .(Mw, R, p=1000*p/0.01)]
-DT <- RMwTable[ Tn == Tn_TARGET & POE == POE_TARGET][p/sum(p)>=1e-5, .(Mw, R, p)]
+DT <- RMwTable[ Tn == Tn_TARGET & POE == POE_TARGET][p/sum(p)>=1e-4, .(Mw, R, p)]
 
 DATA <- DT[, .(Y = round(Mw,2), X = round(R), Z = p)] #hist#D detects the number of decimals of each scale and reports bins and ticks in the same precision
 
@@ -16,7 +16,7 @@ hist3D(data = DATA,
        color.palette = hcl.pals()[112], 
        yAxis.label = "Magnitude Mw", 
        xAxis.label = "Distance R [km]", 
-       aspect.ratio=list(x = 1, y = 1, z = 1.5),
+       aspect.ratio=list(x = 1, y = 1, z = 1.4),
        xAxis.legend = TRUE, 
        yAxis.legend = TRUE, 
        zAxis.legend = FALSE, 
